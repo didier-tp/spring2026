04ff410b8e043cb64746afabbc51e14d2bdfe8d5
Didier Defrance <didier@d-defrance.fr>
2026-03-17 10:24:55 +0100

    s

--- tp/mars2026/BasicAppliSpringWeb/src/main/java/tp/appliSpring/AppliSpringWeb/entity/Compte.java
+++ tp/mars2026/BasicAppliSpringWeb/src/main/java/tp/appliSpring/AppliSpringWeb/entity/Compte.java
@@ -2,19 +2,17 @@ package tp.appliSpring.AppliSpringWeb.entity;
 
 import jakarta.persistence.*;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-@Getter @Setter @NoArgsConstructor @ToString
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@ToString
 @Entity //entity persistante en base de données
 public class Compte {
 
     @Id //identifiant , clef primaire
     @GeneratedValue(strategy = GenerationType.IDENTITY) //pour auto_incrémentation
     private Long numero;
-    
+
     private String label;
 
     private double solde;
