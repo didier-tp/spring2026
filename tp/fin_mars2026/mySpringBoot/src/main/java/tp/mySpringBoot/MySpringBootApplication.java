55a083d7245aacb2adb6d905644517f584e0cc9f
d <dd>
2026-03-24 15:17:01 +0100

    s

--- tp/fin_mars2026/mySpringBoot/src/main/java/tp/mySpringBoot/MySpringBootApplication.java
+++ tp/fin_mars2026/mySpringBoot/src/main/java/tp/mySpringBoot/MySpringBootApplication.java
@@ -1,15 +1,19 @@
 package tp.mySpringBoot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class MySpringBootApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(MySpringBootApplication.class, args);
-		System.out.println("http://localhost:8080/mySpringBoot");
+		//System.out.println("http://localhost:8080/mySpringBoot");
+		log.info("http://localhost:8080/mySpringBoot");
+		//ou bien log.debug() ou ...
 
 	}
 
