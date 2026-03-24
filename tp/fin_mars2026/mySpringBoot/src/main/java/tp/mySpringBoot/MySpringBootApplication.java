c3519a9c1d1d898f66541450f304bf9b84603e8e
d <dd>
2026-03-24 12:11:09 +0100

    s

---
+++ tp/fin_mars2026/mySpringBoot/src/main/java/tp/mySpringBoot/MySpringBootApplication.java
@@ -0,0 +1,16 @@
+package tp.mySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MySpringBootApplication.class, args);
+		System.out.println("http://localhost:8080/mySpringBoot");
+
+	}
+
+}
