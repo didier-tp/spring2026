3d0e2b68b2014ac865f31c6096ec34267ee369c5
d <dd>
2026-03-26 15:44:00 +0100

    s

--- tp/fin_mars2026/debutAppliSpringWeb/src/main/java/tp/appliSpring/bank/web/api/rest/CompteRestCtrl.java
+++ tp/fin_mars2026/debutAppliSpringWeb/src/main/java/tp/appliSpring/bank/web/api/rest/CompteRestCtrl.java
@@ -8,6 +8,8 @@ import tp.appliSpring.bank.core.model.Compte;
 import tp.appliSpring.bank.core.service.ServiceCompte;
 import tp.appliSpring.bank.persistence.entity.CompteEntity;
 import tp.appliSpring.bank.persistence.repository.CompteRepository;
+import tp.appliSpring.generic.dto.ApiError;
+import tp.appliSpring.generic.exception.EntityNotFoundException;
 
 import java.util.List;
 
@@ -39,7 +41,7 @@ public class CompteRestCtrl {
 		this.serviceCompte = serviceCompte;
 	}
 
-
+   /*
 	//Get By ID
 	//V1 avec DTO et V3 (avec automatisme ExceptionHandler)
 	//declencher en mode GET avec
@@ -49,16 +51,25 @@ public class CompteRestCtrl {
 		return serviceCompte.searchById(numeroCompte);
 		//NB: l'objet retourné sera automatiquement converti au format json
 	}
+*/
 
 
 
-    /*
 	//V2 avec ResponseEntity<?> mais sans ExceptionHandler
 
 	//http://localhost:8181/appliSpring/rest/api-bank/v1/comptes/1 ou 2
 	@GetMapping("/{id}")
-	...
-   */
+	public ResponseEntity<?> getCompteById(@PathVariable("id") String numeroCompte) {
+        try {
+            Compte compte = serviceCompte.searchById(numeroCompte);
+			return ResponseEntity.ok(compte);
+        } catch (EntityNotFoundException e) {
+			//return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					             .body(new ApiError(HttpStatus.NOT_FOUND,"compte inexistant"));
+        }
+	}
+
 
 	//GET Multiple
 	//http://localhost:8181/appliSpring/rest/api-bank/v1/comptes
