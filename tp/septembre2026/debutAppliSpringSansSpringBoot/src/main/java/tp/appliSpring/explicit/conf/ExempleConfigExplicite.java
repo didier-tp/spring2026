5f19500342f9d453c38d931aa4cad7a3e7f4a9c4
Didier Defrance <didier@d-defrance.fr>
2026-09-14 16:12:19 +0200

    s

--- tp/septembre2026/debutAppliSpringSansSpringBoot/src/main/java/tp/appliSpring/explicit/conf/ExempleConfigExplicite.java
+++ tp/septembre2026/debutAppliSpringSansSpringBoot/src/main/java/tp/appliSpring/explicit/conf/ExempleConfigExplicite.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import tp.appliSpring.explicit.beans.*;
 
 @Configuration
+@PropertySources({
+        @PropertySource("classpath:exemples.properties")
+       /* ,  @PropertySource("classpath:xyz.properties")*/
+})
 public class ExempleConfigExplicite {
 
     //A faire en Tp:
@@ -21,21 +25,38 @@ public class ExempleConfigExplicite {
     //et vraiante ...Maj si présence du profile "maj"
     //tester le comportement en activant ou pas de profile "maj" en début de main()
 
+    @Value("${preferences.prefixe:##}")
     private String monPrefixe="#";
 
-
+    @Value("${preferences.suffixe:##}")
     private String monSuffixe="#";
 
     @Bean
+    @Profile("!maj")
     public Prefixeur prefixeur(){
         return new PrefixeurBasic(monPrefixe);
     }
 
-
     @Bean
+    @Profile("!maj")
     public Suffixeur suffixeur(){
         return new SuffixeurBasic(monSuffixe);
     }
+
+    @Bean
+    @Profile("maj")
+    public Suffixeur suffixeurMaj(){
+        return new SuffixeurMaj(monSuffixe);
+    }
+
+    @Bean
+    @Profile("maj")
+    public Prefixeur prefixeurMaj(){
+        return new PrefixeurMaj(monPrefixe);
+    }
+
+
+
 
     @Bean
     public Encadreur encadreurSpring( Prefixeur prefixeurSpring , Suffixeur suffixeurSpring){
