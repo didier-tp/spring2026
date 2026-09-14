bbe797f64b4a9174ea4f55811e5fd3b02d12eff9
Didier Defrance <didier@d-defrance.fr>
2026-09-14 14:22:00 +0200

    s

--- tp/septembre2026/debutAppliSpringSansSpringBoot/src/main/java/tp/appliSpring/exemple/MonAfficheurV2.java
+++ tp/septembre2026/debutAppliSpringSansSpringBoot/src/main/java/tp/appliSpring/exemple/MonAfficheurV2.java
@@ -1,10 +1,14 @@
 package tp.appliSpring.exemple;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 //@Primary
+//@Scope("singleton") //"singleton" par défaut
+//@Lazy //très rare , pour différer l'instantiation au premier besoi
 public class MonAfficheurV2 implements MonAfficheur {
 
 	@Override
