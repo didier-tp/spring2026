26e1d7c09f573f510a49d3d45409cff3ffd67f6f
Didier Defrance <didier@d-defrance.fr>
2026-09-14 13:57:43 +0200

    s

---
+++ tp/septembre2026/debutAppliSpringSansSpringBoot/src/main/java/tp/appliSpring/exemple/Coordinateur.java
@@ -0,0 +1,33 @@
+package tp.appliSpring.exemple;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Coordinateur {
+	
+	@Autowired
+	private MonAfficheur monAfficheur=null; //référence vers afficheur à injecter
+	
+	@Autowired
+	private MonCalculateur monCalculateur=null;//référence vers calculateur à injecter
+
+	public Coordinateur(){
+		System.out.println("dans constructeur de Coordinateur, monAfficheur="+this.monAfficheur);
+		System.out.println("dans constructeur de Coordinateur, monCalculateur="+this.monCalculateur);
+	}
+
+	@PostConstruct
+	public void init(){
+		System.out.println("dans init() de Coordinateur, monAfficheur="+this.monAfficheur);
+		System.out.println("dans init() avec @PostContruct, monCalculateur="+this.monCalculateur);
+	}
+	
+
+	public void calculerEtAfficher() {
+		double x=4;
+		double res =monCalculateur.calculer(x); //x*x ou bien 2*x ou bien ...
+		monAfficheur.afficher("res="+res);// >> res=16 en v1 ou bien ** res=16
+	}
+}
