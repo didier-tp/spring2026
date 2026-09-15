0f4b9dd04b58ea6ed6f93a2b0f255d220c9f1a8c
Didier Defrance <didier@d-defrance.fr>
2026-09-15 09:54:12 +0200

    s

--- tp/septembre2026/debutAppliSpringSansSpringBoot/src/main/java/tp/appliSpring/exemple/MonAfficheurV1.java
+++ tp/septembre2026/debutAppliSpringSansSpringBoot/src/main/java/tp/appliSpring/exemple/MonAfficheurV1.java
@@ -1,11 +1,13 @@
 package tp.appliSpring.exemple;
 
 import org.springframework.stereotype.Component;
+import tp.appliSpring.annotation.LogExecutionTime;
 
 @Component
 public class MonAfficheurV1 implements MonAfficheur {
 
 	@Override
+	@LogExecutionTime
 	public void afficher(String message) {
 		System.out.println(">>"+message);
 
--- tp/septembre2026/debutAppliSpringSansSpringBoot/src/main/java/tp/appliSpring/exemple/MonCalculateurCarre.java
+++ tp/septembre2026/debutAppliSpringSansSpringBoot/src/main/java/tp/appliSpring/exemple/MonCalculateurCarre.java
@@ -1,11 +1,15 @@
 package tp.appliSpring.exemple;
 
 import org.springframework.stereotype.Component;
+import tp.appliSpring.annotation.Aff;
+import tp.appliSpring.annotation.LogExecutionTime;
 
 @Component
+//@Aff
 public class MonCalculateurCarre implements MonCalculateur {
 
 	@Override
+	@LogExecutionTime
 	public double calculer(double x) {
 		return x*x;
 	}
