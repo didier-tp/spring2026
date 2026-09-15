fe96651a2f26632dcc189cf46f57febf83494a68
Didier Defrance <didier@d-defrance.fr>
2026-09-15 15:45:27 +0200

    s

--- tp/septembre2026/appliSpring/src/test/java/tp/appliSpring/repository/TestCompteRepository.java
+++ tp/septembre2026/appliSpring/src/test/java/tp/appliSpring/repository/TestCompteRepository.java
@@ -9,6 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import tp.appliSpring.entity.CompteEntity;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 @ActiveProfiles({ "dev"})  //pour analyser application-dev.properties
 @Slf4j
@@ -28,5 +32,16 @@ public class TestCompteRepository {
         Assertions.assertEquals("compteA",compteRelu.getLabel());
         Assertions.assertEquals(100.0,compteRelu.getSolde());
         log.debug("compteRelu apres insertion=" + compteRelu);
+
+        //tester findBySoldeGreaterThanEqual() ou findBySoldeMini():
+        this.compteRepository.save(new CompteEntity(null,"compteB",150.0));
+        this.compteRepository.save(new CompteEntity(null,"compteC",80.0));
+        this.compteRepository.save(new CompteEntity(null,"compteD",60.0));
+
+        List<CompteEntity> compteAvecSoldeAuMoins100 = this.compteRepository.findBySoldeGreaterThanEqual(100.0);
+        //List<CompteEntity> compteAvecSoldeAuMoins100 = this.compteRepository.findBySoldeMini(100.0); //ok
+        assertTrue(compteAvecSoldeAuMoins100.size()>=2);
+        log.debug("compteAvecSoldeAuMoins100=" + compteAvecSoldeAuMoins100);
+
     }
 }
