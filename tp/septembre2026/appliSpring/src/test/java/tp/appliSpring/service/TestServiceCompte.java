f53b25397a106d2ab332228aef759bffc0216b74
Didier Defrance <didier@d-defrance.fr>
2026-09-16 16:15:51 +0200

    s

--- tp/septembre2026/appliSpring/src/test/java/tp/appliSpring/service/TestServiceCompte.java
+++ tp/septembre2026/appliSpring/src/test/java/tp/appliSpring/service/TestServiceCompte.java
@@ -29,4 +29,28 @@ public class TestServiceCompte {
         log.debug("compteRelu apres insertion=" + compteRelu);
     }
 
+    @Test
+    public void testVirement() {
+        CompteEntity compteASauvegarde = this.serviceCompte.saveOrUpdate( new CompteEntity(null,"compteA",300.0));
+        CompteEntity compteBSauvegarde = this.serviceCompte.saveOrUpdate( new CompteEntity(null,"compteB",100.0));
+        Long numCptA = compteASauvegarde.getNumero();
+        Long numCptB = compteBSauvegarde.getNumero();
+        //remonter en memoire les anciens soldes des compte A et B avant virement
+        //(+affichage console ou logger) :
+        double soldeA_avant= compteASauvegarde.getSolde();
+        double soldeB_avant = compteBSauvegarde.getSolde();
+        log.debug("avant bon virement, soldeA_avant="+soldeA_avant + " et soldeB_avant=" + soldeB_avant);
+        //effectuer un virement de 50 euros d'un compte A vers vers compte B
+        this.serviceCompte.transferer(50.0, numCptA, numCptB);
+        //remonter en memoire les nouveaux soldes des compte A et B apres virement // (+affichage console ou logger)
+        CompteEntity compteAReluApresVirement = this.serviceCompte.searchById(numCptA);
+        CompteEntity compteBReluApresVirement = this.serviceCompte.searchById(numCptB);
+        double soldeA_apres = compteAReluApresVirement.getSolde();
+        double soldeB_apres = compteBReluApresVirement.getSolde();
+        log.debug("apres bon virement, soldeA_apres="+soldeA_apres + " et soldeB_apres=" + soldeB_apres);
+        //verifier -50 et +50 sur les différences de soldes sur A et B :
+        Assertions.assertEquals(soldeA_avant - 50, soldeA_apres,0.000001);
+        Assertions.assertEquals(soldeB_avant + 50, soldeB_apres,0.000001);
+    }
+
 }
