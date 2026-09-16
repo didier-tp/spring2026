515cdbd2911242af6730e94c4212e1ffcd46ae1f
Didier Defrance <didier@d-defrance.fr>
2026-09-16 10:29:00 +0200

    s

--- tp/septembre2026/appliSpring/src/main/java/tp/appliSpring/entity/OperationEntity.java
+++ tp/septembre2026/appliSpring/src/main/java/tp/appliSpring/entity/OperationEntity.java
@@ -1,11 +1,52 @@
 package tp.appliSpring.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name="operation")
+@Getter
+@Setter
 public class OperationEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long numero;
+
+
+    @Column(name="label" , length = 64)
+    private String label; //"achat 1" , ...
+
+    private Double montant; //négatif pour un achat/debit , positif pour un credit
+
+    @Column(name="date_op")
+    private LocalDate dateOp;
+
+    @ManyToOne
+    @JoinColumn(name = "num_compte") //nom clef etrangére
+    //@JsonIgnore
+    private CompteEntity compte;
+
+    @Override
+    public String toString() {
+        return "OperationEntity{" +
+                "numero=" + numero +
+                ", label='" + label + '\'' +
+                ", montant=" + montant +
+                ", dateOp=" + dateOp +
+                '}';
+    }
+
+    public OperationEntity(Long numero, String label, Double montant, LocalDate dateOp, CompteEntity compte) {
+        this.numero = numero;
+        this.label = label;
+        this.montant = montant;
+        this.dateOp = dateOp;
+        this.compte = compte;
+    }
+
+    public OperationEntity() {
+    }
 }
