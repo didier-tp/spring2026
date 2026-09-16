37ca79174d4f39e26a19951f44c62376cfe1c0e8
Didier Defrance <didier@d-defrance.fr>
2026-09-16 11:38:56 +0200

    s

--- tp/septembre2026/appliSpring/src/main/java/tp/appliSpring/reinit/ReinitDefaultDataSet.java
+++ tp/septembre2026/appliSpring/src/main/java/tp/appliSpring/reinit/ReinitDefaultDataSet.java
@@ -1,23 +1,32 @@
 package tp.appliSpring.reinit;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import tp.appliSpring.entity.CompteEntity;
+import tp.appliSpring.entity.OperationEntity;
 import tp.appliSpring.repository.CompteRepository;
+import tp.appliSpring.repository.OperationRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Component
 @Profile("reInit")
+@RequiredArgsConstructor
 public class ReinitDefaultDataSet {
 
     private final CompteRepository compteRepository;
+    private final OperationRepository operationRepository;
 
-    public ReinitDefaultDataSet(CompteRepository compteRepository) {
-        this.compteRepository=compteRepository;
-        this.compteRepository.save(new CompteEntity(null,"compteAaa",50.0));
-        this.compteRepository.save(new CompteEntity(null,"compteBbb",60.0));
+    @PostConstruct
+    public void reinitDataSet() {
+        CompteEntity compteA= this.compteRepository.save(new CompteEntity(null,"compteAaa",50.0));
+        CompteEntity compteB= this.compteRepository.save(new CompteEntity(null,"compteBbb",60.0));
+
+        operationRepository.save(new OperationEntity(null,"achat1",-6.6 , LocalDate.now(),compteA));
+        operationRepository.save(new OperationEntity(null,"achat2",-7.6 , LocalDate.now(),compteA));
         //pour verifier:
         List<CompteEntity> comptes = this.compteRepository.findAll();
         System.out.println("ReinitDefaultDataSet ,comptes" + comptes);
