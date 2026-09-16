c5e71152c1d366a1c2ba16e80868422b6fe5fc6d
Didier Defrance <didier@d-defrance.fr>
2026-09-16 16:25:16 +0200

    s

--- tp/septembre2026/appliSpring/src/test/java/tp/appliSpring/service/TestServiceCompte.java
+++ tp/septembre2026/appliSpring/src/test/java/tp/appliSpring/service/TestServiceCompte.java
@@ -53,4 +53,34 @@ public class TestServiceCompte {
         Assertions.assertEquals(soldeB_avant + 50, soldeB_apres,0.000001);
     }
 
+    //Le test testMauvaisVirement() doit normalement réussir avec @Transactional sur transferer() dans ServiceCompteImpl
+    //et echouer sinon
+    @Test
+    public void testMauvaisVirement() {
+        CompteEntity compteASauvegarde = this.serviceCompte.saveOrUpdate( new CompteEntity(null,"compteA",300.0));
+        CompteEntity compteBSauvegarde = this.serviceCompte.saveOrUpdate( new CompteEntity(null,"compteB",100.0));
+        Long numCptA = compteASauvegarde.getNumero();
+        Long numCptB = compteBSauvegarde.getNumero();
+        //remonter en memoire les anciens soldes des compte A et B avant virement
+        //(+affichage console ou logger) :
+        double soldeA_avant= compteASauvegarde.getSolde();
+        double soldeB_avant = compteBSauvegarde.getSolde();
+        log.debug("avant mauvais virement, soldeA_avant="+soldeA_avant + " et soldeB_avant=" + soldeB_avant);
+        //effectuer un virement de 50 euros d'un compte A vers vers compte -1 qui n'existe pas
+        try {
+            this.serviceCompte.transferer(50.0, numCptA, -1);
+        } catch (Exception e) {
+            System.err.println("erreur attendue:" + e.getMessage()); //exception normale/attendue car -1 n'existe pas
+        }
+        //remonter en memoire les nouveaux soldes des compte A et B apres virement // (+affichage console ou logger)
+        CompteEntity compteAReluApresVirement = this.serviceCompte.searchById(numCptA);
+        CompteEntity compteBReluApresVirement = this.serviceCompte.searchById(numCptB);
+        double soldeA_apres = compteAReluApresVirement.getSolde();
+        double soldeB_apres = compteBReluApresVirement.getSolde();
+        log.debug("apres mauvais virement, soldeA_apres="+soldeA_apres + " et soldeB_apres=" + soldeB_apres);
+        //verifier aucunes différences de soldes sur A et B (car si rollback bien fait rien doit changer):
+        Assertions.assertEquals(soldeA_avant , soldeA_apres,0.000001);
+        Assertions.assertEquals(soldeB_avant , soldeB_apres,0.000001);
+    }
+
 }
