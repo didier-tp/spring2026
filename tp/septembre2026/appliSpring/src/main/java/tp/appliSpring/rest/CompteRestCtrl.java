f3f8c6fa9a5b87d8e34ea451cd3f93231d350b52
Didier Defrance <didier@d-defrance.fr>
2026-09-17 12:07:08 +0200

    s

--- tp/septembre2026/appliSpring/src/main/java/tp/appliSpring/rest/CompteRestCtrl.java
+++ tp/septembre2026/appliSpring/src/main/java/tp/appliSpring/rest/CompteRestCtrl.java
@@ -1,6 +1,8 @@
 package tp.appliSpring.rest;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tp.appliSpring.entity.CompteEntity;
 import tp.appliSpring.mapper.MyMapper;
@@ -18,6 +20,8 @@ public class CompteRestCtrl {
     private final ServiceCompte serviceCompte;
     private final MyMapper myMapper;
 
+    /*
+    //V1
     //http://localhost:8080/appliSpring/rest/bank-api/v1/comptes/1 ou 2
     @GetMapping("/{numCompte}" )
     public Compte getCompteByNum(@PathVariable("numCompte") Long numCompte) {
@@ -25,6 +29,21 @@ public class CompteRestCtrl {
         //Compte compteDto = new Compte(compteEntity.getNumero(),compteEntity.getLabel(),compteEntity.getSolde());
         Compte compteDto = myMapper.compteEntityToCompte(compteEntity);
         return compteDto;
+    }
+    */
+
+    //V2 avec ResponseEntity
+    //http://localhost:8080/appliSpring/rest/bank-api/v1/comptes/1 ou 2
+    @GetMapping("/{numCompte}" )
+    public ResponseEntity<Compte> getCompteByNum(@PathVariable("numCompte") Long numCompte) {
+        CompteEntity compteEntity = null;
+        try {
+            compteEntity = serviceCompte.findById(numCompte).get();
+            Compte compteDto = myMapper.compteEntityToCompte(compteEntity);
+            return new ResponseEntity<Compte>(compteDto, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<Compte>(HttpStatus.NOT_FOUND);//404
+        }
     }
 
     //http://localhost:8080/appliSpring/rest/bank-api/v1/comptes
