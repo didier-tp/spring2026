01b163ae970a1266b4fb30f816ea3e74d654f1df
Didier Defrance <didier@d-defrance.fr>
2026-09-18 11:24:11 +0200

    s

--- tp/septembre2026/appliSpring/src/main/java/tp/appliSpring/conf/WithSecurityConfig.java
+++ tp/septembre2026/appliSpring/src/main/java/tp/appliSpring/conf/WithSecurityConfig.java
@@ -3,6 +3,8 @@ package tp.appliSpring.conf;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,10 +15,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WithSecurityConfig {
 	
 	@Bean
-	protected SecurityFilterChain withSecurityFilterChain(HttpSecurity http) throws Exception {
+	@Order(1)
+	protected SecurityFilterChain restFilterChain(HttpSecurity http) throws Exception {
 		return http.securityMatcher("/rest/**")
 				.authorizeHttpRequests(
-						auth -> auth.requestMatchers("/**").permitAll()
+						auth -> auth
+								.requestMatchers(HttpMethod.GET,"/rest/api-bank/v1/comptes/**").permitAll()
+								.requestMatchers("/rest/api-bank/v1/comptes/**").authenticated()
 				)
 				.cors( Customizer.withDefaults())
 				.csrf( csrf -> csrf.disable() )
@@ -24,5 +29,15 @@ public class WithSecurityConfig {
 				.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
 				.build() ;
 	}
-	
+
+	@Bean
+	@Order(99)
+	protected SecurityFilterChain otherFilterChain(HttpSecurity http) throws Exception {
+		return http.securityMatcher("/**")
+				.authorizeHttpRequests(
+						auth -> auth
+								.requestMatchers("/**").permitAll()
+				).build() ;
+	}
+
 }
